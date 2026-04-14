9bf65536aafea97338ec6a63d5ab6a22a5979cde
Kumar Gaurav <kumar@make.io>
2026-04-14 17:25:07 +0530

    added sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://ggmakeupworld.com',
+  generateRobotsTxt: true, // generates robots.txt automatically
+  sitemapSize: 7000,
+}
